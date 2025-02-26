1c98e65fc48f2dfd53ce21421890a1bd558c6452
Isooba Mbeiza Rachel <123367383+Shiramiki@users.noreply.github.com>
2025-02-26 10:50:50 +0300

    classwork

---
+++ 26-02-25/Classwork.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE Class;
+-- Check Constraint
+USE Class; 
+
+DROP TABLE Employee;
+CREATE TABLE Employee(
+EmployeeeID INT PRIMARY KEY AUTO_INCREMENT,
+FirstName VARCHAR(50) NOT NULL,
+LastName VARCHAR(50) NOT NULL,
+Age INT,
+CONSTRAINT Employee_AgeCheck CHECK(Age>=18), 
+Email VARCHAR(100) UNIQUE
+);
+
+select CONSTRAINT_NAME, CONSTRAINT_type FROM information_schema.TABLE_CONSTRAINTS WHERE TABLE_NAME='Employee';
+INSERT INTO Employee (FirstName, LastName, Age, Email) VALUES
+("John", "Cat", 30, "JohnCat@gmail.com"),
+("Jane", "Doe", 25, "Jane.doe@gmail.com"),
+("Daniel", "Wilson", 35, "danielwilson@gmail.com");
+
+ALTER Table Employee Drop CONSTRAINT Employee_AgeCheck;
+
